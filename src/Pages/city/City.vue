76b985da5c00f5a07e4e07eb2c2fc610b9858e34
tyler4400 <tyler4400@foxmail.com>
2018-09-14 15:16:51 +0800

    完成城市页面的header和搜索框

---
+++ src/Pages/city/City.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <city-header></city-header>
+        <city-search></city-search>
+    </div>
+</template>
+
+<script>
+    import CityHeader from './component/Header'
+    import CitySearch from './component/Search'
+    export default {
+        name: 'City',
+        components: {
+            cityHeader: CityHeader,
+            CitySearch
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
