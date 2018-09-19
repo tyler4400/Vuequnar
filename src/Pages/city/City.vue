50065bc69c92264ee1069a494a4c3012deda3d8e
tyler4400 <tyler4400@foxmail.com>
2018-09-19 16:26:33 +0800

    城市列表开发完成

--- src/Pages/city/City.vue
+++ src/Pages/city/City.vue
@@ -2,17 +2,48 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities"></city-alphabet>
     </div>
 </template>
 
 <script>
-    import CityHeader from './component/Header'
-    import CitySearch from './component/Search'
+    import CityHeader from './component/Header';
+    import CitySearch from './component/Search';
+    import CityList from './component/List';
+    import CityAlphabet from './component/Alphabet';
+    import axios from 'axios';
+
     export default {
         name: 'City',
         components: {
             cityHeader: CityHeader,
-            CitySearch
+            CitySearch,
+            CityList,
+            CityAlphabet
+        },
+        data () {
+            return{
+                cities: [],
+                hotCities: [],
+            }
+        },
+        methods: {
+            getCityInfo () {
+                axios.get('/api/city.json')
+                    .then(this.handleGetCityInfoSucc);
+            },
+            handleGetCityInfoSucc (res) {
+                res = res.data;
+                if(res.ret && res.data){
+                    const data = res.data;
+                    this.hotCities = data.hotCities;
+                    this.cities = data.cities;
+                }
+            }
+        },
+        mounted () {
+            this.getCityInfo();
         }
     }
 </script>
