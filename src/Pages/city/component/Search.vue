e97163cd903f2901e5e8e8d44a48c81d2139f95f
tyler4400 <tyler4400@foxmail.com>
2018-09-30 10:23:27 +0800

    '完成搜索逻辑实现'

--- src/Pages/city/component/Search.vue
+++ src/Pages/city/component/Search.vue
@@ -1,13 +1,62 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+    <div>
+        <div class="search">
+            <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
+        </div>
+        <div class="search-content" v-show="keyword" ref="wrapper">
+            <ul>
+                <li class="search-item" v-for="item in list" :key="item.id">{{item.name}}</li>
+                <li class="search-item" v-show="noData">无匹配数据</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
-  export default {
-    name: 'Search'
-  }
+    import BScroll from 'better-scroll'
+    export default {
+        name: 'Search',
+        props: {
+            cities: [Object, Array],
+        },
+        data () {
+            return {
+                keyword: '',
+                list: [],
+                timer: null
+            };
+        },
+        computed: {
+            noData () {
+                return !this.list.length;
+            }
+        },
+        watch: {
+            keyword () {
+                if (this.timer){
+                    clearTimeout(this.timer);
+                }
+                if (!this.keyword) {
+                    this.list = []
+                    return
+                }
+                this.timer = setTimeout(() => {
+                const result = [];
+                for (let i in this.cities){
+                    this.cities[i].forEach(value => {
+                        if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1){
+                            result.push(value);
+                        }
+                    })
+                }
+                this.list = result;
+                }, 300)
+            }
+        },
+        mounted () {
+            this.scroll = new BScroll(this.$refs.wrapper)
+        }
+    }
 </script>
 
 <style lang="stylus" scoped>
@@ -24,5 +73,19 @@
             line-height: .62rem
             text-align: center
             border-radius: .06rem
+            color: #666
+    .search-content
+        z-index: 1
+        overflow: hidden
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        background: #eee
+        .search-item
+            line-height: .62rem
+            padding-left: .2rem
+            background: #fff
             color: #666
 </style>
