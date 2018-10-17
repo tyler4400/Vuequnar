4ba28f22f281277dd45ce115a935f00581b5570e
tyler4400 <tyler4400@foxmail.com>
2018-10-17 20:50:26 +0800

    9-7 Vue项目详情页 - 在项目中加入基础动画

--- src/Pages/detail/component/Banner.vue
+++ src/Pages/detail/component/Banner.vue
@@ -12,20 +12,24 @@
                 </div>
             </div>
         </div>
-        <common-gallary
-            :imgs="imgs"
-            v-show="showGallary"
-            @close="handleGallaryClose"
-        ></common-gallary>
+        <fade-animation>
+            <common-gallary
+                :imgs="imgs"
+                v-show="showGallary"
+                @close="handleGallaryClose"
+            ></common-gallary>
+        </fade-animation>
     </div>
 </template>
 
 <script>
     import CommonGallary from '@/common/gallary/Gallary';
+    import FadeAnimation from '@/common/fade/FadeAnimation';
     export default {
         name: 'Detail',
         components: {
-            CommonGallary
+            CommonGallary,
+            FadeAnimation
         },
         props: {
             sightName: String,
